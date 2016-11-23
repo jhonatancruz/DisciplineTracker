9af959e17a46484b068c84f9033cdfdc1b23d2af
Jhonatan Cruz <jcruz3@drew.edu>
2016-11-23 00:15:24 -0500

    updated so that screenshots first and then images

--- motivate.py
+++ motivate.py
@@ -2,20 +2,27 @@ import os
 import pyautogui
 import time
 
-os.system('ls')
-os.system('sh photoBooth.sh')
-time.sleep(3)
 
+date_string = time.strftime("%Y-%m-%d-%H:%M")
+image_name= date_string+'.png'
+im2 = pyautogui.screenshot(image_name)
+os.system('mv *.png images/screenShots')
+# os.system('mv re')
+
+os.system('sh photoBooth.sh')
+time.sleep(2)
 
 pyautogui.keyDown('option')
 pyautogui.keyDown('command')
 pyautogui.press('t')
 pyautogui.keyUp('option')
 pyautogui.keyUp('command')
-time.sleep(3)
+time.sleep(2)
 
-pyautogui.press('left')
-time.sleep(1)
+pyautogui.keyDown('command')
+pyautogui.press('1')
+pyautogui.keyUp('command')
+
 #copy image
 pyautogui.keyDown('command')
 pyautogui.press('c')
@@ -25,8 +32,14 @@ time.sleep(1)
 pyautogui.keyDown('command')
 pyautogui.press('q')
 pyautogui.keyUp('command')
+time.sleep(1)
 
+os.system('open /Users/jhonatancruz/Desktop/Github/DisciplineTracker/images/youImages')
 #paste image in desktop
 pyautogui.keyDown('command')
 pyautogui.press('v')
+pyautogui.keyUp('command')
+
+pyautogui.keyDown('command')
+pyautogui.press('w')
 pyautogui.keyUp('command')
